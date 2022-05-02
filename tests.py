5f7ad9f0d8411370f05c0de4b5d70e541f260fdf
Antonio Ossa Guerra <aaossa@uc.cl>
2022-05-02 12:13:03 -0400

    :white_check_mark: Add tests for Game.winner
    Test different cases that lead to any of both players winning or none
    of them
    
    Signed-off-by: Antonio Ossa Guerra <aaossa@uc.cl>

--- tests.py
+++ tests.py
@@ -25,8 +25,18 @@ def test_available(new_game, one, two, expected):
     assert new_game.available == expected
 
 
-def test_winner():
-    pass
+@mark.parametrize(
+    'one, two, expected', [
+        ([], [], None),
+        ([1, 3, 5], [2, 4, 6], None),
+        ([3, 5, 7], [4, 6], 'Punto'),
+        ([9, 1, 2, 3], [5, 8, 4, 6], 'Rakhi'),
+    ]
+)
+def test_winner(new_game, one, two, expected):
+    new_game.p1.numbers.extend(one)
+    new_game.p2.numbers.extend(two)
+    assert getattr(new_game.winner, 'name', None) == expected
 
 
 def test_fifteen():
